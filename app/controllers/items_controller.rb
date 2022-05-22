e03819473d42b2886ce60f4e4f7fbee9850999a1
keizo0708 <k.kawada0228@gmail.com>
2022-05-22 15:23:22 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,19 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(params_item)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+
   private
 
   def move_to_index
