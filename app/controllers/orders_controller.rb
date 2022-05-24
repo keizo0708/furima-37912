734a436f16e1fe959f8f5fe7f877251c8eb37445
keizo0708 <k.kawada0228@gmail.com>
2022-05-24 17:20:53 +0900

    コントローラーの設定

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,44 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :move_to_root, only: :index
+
+  def index
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      return redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_address).permit(:postal_code, :county_id, :city, :address, :building, :tell).merge(token: params[:token], user_id: current_user.id, item_id: @item.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
+  end
+
+  def move_to_root
+    @item = Item.find(params[:item_id])
+    if @item.user.id == current_user.id || @item.order
+      redirect_to root_path 
+    end
+  end
+
+end
